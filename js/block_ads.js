3973d27d7fdc66d63f1c777e6ee3e5e0e2e3d5eb
saiwei <978447713@qq.com>
2015-06-26 00:13:05 +0800

    version 1.3.1 - 屏蔽右侧全部广告

--- js/block_ads.js
+++ js/block_ads.js
@@ -1,5 +1,23 @@
 
-var x = document.getElementById('ec_im_container')
-x.parentNode.removeChild(x);
 
-console.log('block baidu ads by saiwei')
+var count = 0;
+var x;
+
+
+/**
+ * 屏蔽右侧广告
+ */
+function block_right_ads(){
+    x = document.getElementById('ec_im_container')
+    x.parentNode.removeChild(x);
+
+    x = document.getElementsByClassName('ec-figcaption')[0]
+    x.parentNode.removeChild(x);
+}
+
+block_right_ads();
+
+
+console.log('block baidu ads by saiwei  count='+count++)
+
+
