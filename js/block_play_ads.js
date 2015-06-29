424254ec57a3a75edb81bbc2da71cbb3f736f5ba
saiwei <978447713@qq.com>
2015-06-29 09:58:28 +0800

    version 1.5.19 - 屏蔽http://play.baidu.com/ 右侧的广告,和暂停的广告

--- js/block_play_ads.js
+++ js/block_play_ads.js
@@ -3,13 +3,21 @@
  */
 
 var x;
+var count = 0;
+var t;
 
 /**
  * 屏蔽百度音乐右侧广告
  */
 function block_baidu_play_ads(){
 
-    console.log("block_baidu_play_ads()")
+    console.log("block_baidu_play_ads() count＝"+count)
+
+    count++;
+
+    if(count>12){
+        return;
+    }
 
     //屏蔽右侧广告
     x = document.getElementById('right-ads')
@@ -18,8 +26,14 @@ function block_baidu_play_ads(){
     //屏蔽暂停广告
     x = document.getElementById('pauseAd')
     if(x != null) x.parentNode.removeChild(x);
+
+    t = window.setTimeout(block_baidu_play_ads, 300)
+}
+
+if(t != null){
+    clearTimeout(t)
 }
 
 setTimeout(function(){
 	block_baidu_play_ads();
-},100);
+},300);
